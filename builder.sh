6512b394a0199494265d4312f1595895ed0cfe37
An Nguyen <nthienan.it@gmail.com>
2019-04-08 15:12:31 +0700

    alpine image

---
+++ builder.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+MODE="$1"
+IMAGE="$2"
+VERSION="$3"
+DIR="$4"
+
+cd $DIR
+
+if [ "$MODE" = 'build' ]; then
+    echo -e "\nBuilding $IMAGE:$VERSION..."
+    docker pull $IMAGE:$VERSION || true
+    docker build --pull --cache-from $IMAGE:$VERSION -t $IMAGE:$VERSION .
+fi
+if [ "$MODE" = "deploy" ]; then
+    echo -e '\nDeploying...'
+    docker push $IMAGE:$VERSION
+fi
